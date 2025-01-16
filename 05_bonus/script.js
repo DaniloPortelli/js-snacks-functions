a9585dd7e569f11e480cfd6bc269ccc6ed4a0f8c
DaniloPortelli <daniloportelli@gmail.com>
2025-01-16 15:12:40 +0100

    invio esercizio bonus

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -1,4 +1,5 @@
-/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito. 
+/* Scrivi una funzione che accetti una stringa contenente un nome e 
+restituisca un saluto seguito dal nome fornito. 
 Il saluto deve essere :
 buongiorno se è mattina (fino alle 13), 
 buon pomeriggio se è pomeriggio (fino alle 17) 
@@ -7,11 +8,28 @@ e buonasera se è sera (oltre le 17)
 
 const name = 'Mario';
 
+let date = new Date
+// console.log(date)
+
+let hour = date.getHours();
+// console.log(hour)
+
 
 // Dichiara la funzione qui.
+function salutoUserName(parola2) {
 
+    let res = (hour <= 13) ? "Buongiorno " + (parola2) :
+                (hour <= 17) ? "Buon pomeriggio " + (parola2):
+                (hour > 17) ? "Buonasera " + (parola2): "";
+        console.log(res)
+
+    return parola2;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+(salutoUserName(name))
+
+
 
 
 
